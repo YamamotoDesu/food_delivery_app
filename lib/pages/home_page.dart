0e9b24303d095d8e3e26819b13420f190b84ae46
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-03-24 17:47:27 +0900

    25:14

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/pages/my_silver.app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +13,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: const Text(
+              "title",
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+
+                // my current location
+
+                // description box
+              ],
+            ),
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
       ),
-      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/pages/my_silver.app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MySilverAppBar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+
+  const MySilverAppBar({
+    super.key,
+    required this.child,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 348,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        // cart button
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_cart),
+        )
+      ],
+      backgroundColor: Theme.of(context).colorScheme.background,
+      foregroundColor: Theme.of(context).colorScheme.primary,
+      title: const Text("Sunset Dinner"),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+        expandedTitleScale: 1,
+      ),
+    );
+  }
+}
